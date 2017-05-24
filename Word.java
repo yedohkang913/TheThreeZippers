0821d93fd3a509ffb951b0a486ce316b50453d66
Kristin Lin <klin11@stuy.edu>
2017-05-23 23:24:55 -0400

    added some comments and improved readability

--- Word.java
+++ Word.java
@@ -23,10 +23,41 @@ public class Word implements Comparable{
 	lastWord = null;
     }
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~COMPARABLE METHODS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+    // compareTo - looks at timesMissed
     public int compareTo( Object w ){
         return this.getTimesMissed() - ((Word) w).getTimesMissed();
     }
 
+    /* alphaCompare - for alphabetizing
+      returns - if this word comes before the argument in the dictionary
+      returns 0 if they are the same word
+      returns + if this word comes after the argument in the dictionary
+     */
+    public int alphaCompare(Object that) {
+	if (name.equals(((Word) that).getName())) {return 0;}
+	int a = 0; int b = 0;
+	// while there are still letters to compare
+	while (a != name.length() && b != ((Word) that).getName().length()) {
+	    // if their letter is the same, move on to next letter
+	    if (ALPHA.indexOf(name.substring(a,a+1)) ==
+		ALPHA.indexOf(((Word) that).getName().substring(b, b+1))) {
+		a++; b++; 
+	    } // otherwise compare where those letters stand in the alphabet
+	    else {
+		return ALPHA.indexOf(name.substring(a,a+1)) -
+		    ALPHA.indexOf(((Word) that).getName().substring(b, b+1));
+	    }
+	} 
+	// one word must exist in the other; shorter letter comes first
+	return name.length() - ((Word) that).getName().length();
+    }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
+//~~~~~~~~~~~~~~~~~~~~~~~ ACCESSORS & MODIFIERS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
     public String getName() {
         return name;
     }
@@ -67,27 +98,7 @@ public class Word implements Comparable{
 	return name;
     }
 
-    /*
-      returns - if this word comes before the argument in the dictionary
-      returns 0 if they are the same word
-      returns + if this word comes after the argument in the dictionary
-     */
-    public int alphaCompare(Object that) {
-	if (name.equals(((Word) that).getName())) {return 0;}
-	int a = 0; int b = 0;
-	while (a != name.length() && b != ((Word) that).getName().length()) {
-	    if (ALPHA.indexOf(name.substring(a,a+1)) ==
-		ALPHA.indexOf(((Word) that).getName().substring(b, b+1))) {
-		a++;
-		b++;
-	    }
-	    else {
-		return ALPHA.indexOf(name.substring(a,a+1)) -
-		    ALPHA.indexOf(((Word) that).getName().substring(b, b+1));
-	    }
-	}
-	return name.length() - ((Word) that).getName().length();
-    }
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //tester for alphaCompare
     public static void main(String[] args) {
