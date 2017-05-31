b1303bc7fa0559b60a2d6bed8fbee53a8accd0d9
kristinlin <klin11@stuy.edu>
2017-05-30 23:09:23 -0400

    added inline comments

--- Matching.java
+++ Matching.java
@@ -1,17 +1,22 @@
 /*========================
   class Matching
+  Prints words and definitions in two columns and requires users to input the 
+  corresponding letters and numbers to match the words to their definitions.
   ==========================*/
 
 import java.util.Scanner;
 
 public class Matching {
- 
+
+    //~~~~~~~~~instance vars~~~~~~~~~~~~~
     private Word[] wordDeck;
     private Word[] defDeck;
     private int numMatched;
     private Word[][] display;
     private final String ALPHA = "abcdefghijklmnopqrstuvwxyz";
- 
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //constructor
     public Matching (Set newDeck) {
 	System.out.println("Welcome to Matching!");
 
@@ -45,7 +50,8 @@ public class Matching {
 	numMatched = 0;
 	
     }// end constructor
- 
+
+    
     // prints the word in one column and the def in another; 
     // if there is a null pointer, skip this line
     public void printDisplay() {
@@ -56,22 +62,28 @@ public class Matching {
 	    if (display[r][1] != null) 
 		output2 += + (r+1) + ": "  + display[r][1].getDefinition();
 	    System.out.print("\t");
+	    //make these outputs each a minimum of 26 spaces
 	    System.out.printf("%-26s", output1);
 	    System.out.print("\t");
 	    System.out.printf("%-26s", output2);
 	    System.out.println();
 	}
     }
+    
  
     public void play() {
 	Scanner sc1 = new Scanner(System.in);
 	Scanner sc2 = new Scanner(System.in);
 	System.out.println("LET'S STUDY WITH MATCHING!");
 	System.out.println("Type in the letter corresponding to a word, then type in the number corresponding to its definition!");
+
+	//numMatched increases until all matches are made
 	while (numMatched != wordDeck.length) {
-	    printDisplay();
+	    
+	    printDisplay(); //print the columns
 	    String input = sc1.next().toLowerCase();
-	    //make the input readable
+	    
+	    //make the input readable--separate letters and numbers
 	    String  wordPlace = ""; String defPlace = "";
 	    for (int x = 0; x < input.length(); x++) {
 		if (ALPHA.indexOf(input.substring(x, x+1)) != -1) {
@@ -80,6 +92,7 @@ public class Matching {
 		    defPlace += input.substring(x, x+1);
 		}
 	    }
+	    
 	    //is it a valid input?
 	    if ( wordPlace == "" || defPlace == "") {
 		System.out.println("Input does not follow instructions.");
@@ -87,12 +100,15 @@ public class Matching {
 	    else {
 		int indexOfWord = Integer.parseInt(wordPlace);
 		int indexOfDef = Integer.parseInt(defPlace) - 1;
+
+		//is the input within indices and does the word exist?
 		if (indexOfWord < 0 || indexOfWord >= wordDeck.length ||
 		    indexOfDef < 0 || indexOfDef >= wordDeck.length ||
 		    display[indexOfWord][0] == null ||
 		    display[indexOfDef][1] == null) {
 		    System.out.println("Input does not follow instructions.");
 		}
+		
 		//is the input correct?
 		else if (display[indexOfWord][0].getDefinition().equals(
 			 display[indexOfDef][1].getDefinition())) {
@@ -101,7 +117,8 @@ public class Matching {
 		    display[indexOfWord][0] = null;
 		    display[indexOfDef][1] = null;
 		}
-		else { //input is wrong
+		
+		else { //input is wrong & numMatched does not change
 		    System.out.println("Sorry, buddy.");
 		}
 	    }
