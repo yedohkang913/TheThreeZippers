0b1b888ed08f2557216ca2d69aa4de62ae867339
kristinlin <klin11@stuy.edu>
2017-05-30 22:36:42 -0400

    under construction

--- Driver.java
+++ Driver.java
@@ -21,69 +21,75 @@ public class Driver {
 			       "\n3: Play a game with an existing set." +
 			       "\n4: Exit");
 	    pass = false;
-	    int option;
+	    int option = 0;
 	    while (!pass) {
 		try {
 		    option = sc.nextInt();
-		    pass = true;
-		} catch (Exception e) {
-		    System.out.println("Enter a valid input.");
-		}
+		    if (option <= 4) {
+			pass = true;
+		    } else {
+			System.out.println("Bad input");
+		    }
+	     	} catch (Exception e) {
+	     	    System.out.println("Enter a valid input.");
+	     	}
 	    }
 	    if (option == 1 ) {
-		System.out.println("\nWelcome to the DICTIONARY." +
-				   "\nEnter H at any time to return home" +
-				   "\nOtherwise, enter a word for its definition.");
-		String input = sc.next();
-		if (input.equals("H")) {
-		    //do nothing; loops again to main menu
-		}
-		else {
-		    System.out.println("Currently under construction, sorry.");
+	     	System.out.println("\nWelcome to the DICTIONARY." +
+	     			   "\nEnter H at any time to return home" +
+	     			   "\nOtherwise, enter a word for its definition.");
+	     	String input = sc.next();
+	     	if (input.equals("H")) {
+	     	    //do nothing; loops again to main menu
+	     	}
+	     	else {
+	     	    System.out.println("Currently under construction, sorry.");
 
-		}
+	     	}
 	    }
 	    else if (option == 2) {
-		System.out.println("A name for your new Set: ");
-		_all.add(new Set (sc.next()));
-		System.out.println("Enter words or S to save and exit.");
-		String input = sc.next().toLowerCase();
-		while (!input.equals("s")) {
-		    System.out.println("Dictionary is still under construction.");
-		    //_all[_all.size()-1].add(input);
-		    input = sc.next().toLowerCase();
-		}
+	     	System.out.println("A name for your new Set: ");
+	     	_all.add(new Set (sc.next()));
+	     	System.out.println("Enter words or S to save and exit.");
+	     	String input = sc.next().toLowerCase();
+	     	while (!input.equals("s")) {
+	     	    System.out.println("Dictionary is still under construction.");
+	     	    //_all[_all.size()-1].add(input);
+	     	    input = sc.next().toLowerCase();
+	     	}
 	    }
 	    else if (option == 3) {
-		System.out.println("Select a game:");
-		System.out.println("\t1: Flashcards" +
-				   "\t2: Learn" +
-				   "\t3: Matching");
-		pass = false;
-		String option2 = sc.next();
-		if (option2.equals("H")) {
-		    //do nothing--end this round
-		} else {
-		    while (!pass) {
-			try {
-			    int opt = Integer.parseInt(option2);
-			    pass = true;
-			} catch (Exception e) {
-			    System.out.println("Enter a valid input.");
-			}
-		    }
-		    System.out.println("Select an existing Set.");
-		    for (int x = 0; x < _all.size(); x++) {
-			System.out.println("\t" +
-					   (x+1) +
-					   ": " +
-					   _all.get(x).getName());
-		    }
-		    System.out.println("Currently under construction, sorry.");
-		    System.out.println("Here's an example set:");
+	     	System.out.println("Select a game:");
+	     	System.out.println("\t1: Flashcards");
+		System.out.println("\t2: Learn");
+		System.out.println("\t3: Matching");
+
+	     	pass = false;
+	     	String option2 = sc.next();
+	     	if (option2.equals("H")) {
+	     	    //do nothing--end this round
+	     	} else {
+	     	    while (!pass) {
+	     		try {
+	     		    int opt = Integer.parseInt(option2);
+	     		    pass = true;
+	     		} catch (Exception e) {
+	     		    System.out.println("Enter a valid input.");
+	     		}
+	     	    }
+	     	    System.out.println("Select an existing Set.");
+	     	    for (int x = 0; x < _all.size(); x++) {
+	     		System.out.println("\t" +
+	     				   (x+1) +
+	     				   ": " +
+	     				   _all.get(x).getName());
+	     	    }
+	     	    System.out.println("Currently under construction, sorry.");
+	     	    System.out.println("Here's an example set:");
 	
-		    Set hello = new Set("Compliments");
-		    Word cool = new Word("cool","not warm enough");
+	     	    Set hello = new Set("Compliments");
+	     	    Word cool = new Word("cool"
+					 ,"not warm enough");
 		    Word great = new Word ("great","what you say to a mate");
 		    Word superb = new Word ("superb","it's not just super, it's _____");
 		    Word fantastic = new Word ("fantastic","______ Beasts and where to find them.");
