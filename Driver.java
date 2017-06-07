64eb77af089481865b07fc147a1d4e47ad82a775
kristinlin <klin11@stuy.edu>
2017-06-07 01:03:59 -0400

    fixed negative inputs

--- Driver.java
+++ Driver.java
@@ -51,8 +51,8 @@ public class Driver {
 	    while (!pass) {
 		try {
 		    choiceMain = Integer.parseInt(option); //should be an int
-		    //should be less than or equal to 5
-		    if (choiceMain <= 6) { 
+		    //should be less than or equal to 5 but not negative
+		    if (choiceMain <= 6 && choiceMain > -1) { 
 			pass = true; 
 		    }
 		    else {  
@@ -189,7 +189,7 @@ public class Driver {
 		while (!option3.equals("h") && !pass) {
 		    try {
 			editSet = Integer.parseInt(option3);
-			if (editSet < _all.size()) {
+			if (editSet < _all.size() && editSet > -1) {
 			    pass = true;
 			}
 			else {
@@ -222,7 +222,7 @@ public class Driver {
 		    while (!option3.equals("h") && !pass) {
 			try {
 			    how = Integer.parseInt(option3);
-			    if (how <= 4) {
+			    if (how <= 4 && how > -1) {
 				pass = true;
 			    }
 			    else {
@@ -356,7 +356,7 @@ public class Driver {
 		while (!option4.equals("h") && !pass) {
 		    try {
 			game = Integer.parseInt(option4);
-			if ( game <= 3 ) {
+			if ( game <= 3 && game > -1) {
 			    pass = true;
 			}
 			else {
@@ -393,7 +393,7 @@ public class Driver {
 		    while (!option4.equals("h") && !pass) {
 			try {
 			    setChoice = Integer.parseInt(option4);
-			    if (setChoice < _all.size()) {
+			    if (setChoice < _all.size() && setChoice > -1) {
 				pass = true;
 			    }
 			    else {
@@ -452,7 +452,7 @@ public class Driver {
 		while (!sett.equals("h") && !pass) {
 		    try {
 			setC = Integer.parseInt(sett);
-			if (setC < _all.size()) {pass = true;}
+			if (setC < _all.size() && setC > -1) {pass = true;}
 			else {
 			    System.out.print("\nPlease enter a valid input: "); 
 			    sett = sc.next().toLowerCase();}
